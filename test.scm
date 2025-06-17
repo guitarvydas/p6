f25c27dfe5c4d8aa87ba1c84f47ecf70fc0aa5d5
Paul Tarvydas <paul.tarvydas@gmail.com>
2025-06-16 23:04:56 -0400

    &terminator

---
+++ test.scm
@@ -0,0 +1,17 @@
+; `a —> (quote a)
+; `,a —> a
+; `(a) —> (list (quote a))
+; `(a b c) —> (list (quote a) (quote b) (quote c))
+; `(a ,b c) —> (list (quote a) b (quote c))
+; ,a —-> error (comma not inside backquote)
+
+`a
+`,a
+`(a)
+`(a b c)
+`(a ,b c)
+;,a
+'x
+'(x)
+'(x y z)
+(list x 'y z)

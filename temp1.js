fade826138ad6f13cc908701979cf95b95b61b00
Paul Tarvydas <paul.tarvydas@gmail.com>
2025-06-30 17:49:37 -0400

    rm redundant lp/rp from 'bindings' on tojs.rwr

---
+++ temp1.js
@@ -0,0 +1,4 @@
+function clear_r (x) {
+    set-car_B (cddr (x, ), 
+	      [nil, ], );
+}
--- test.scm
+++ test.scm
@@ -1,6 +1,9 @@
 
-(define goals '((some (? X))))
-
-; 9-slide PROVE
-(prove6 '() goals db empty 1 '())
-
+(define (try g r e n)
+  (if (null? r)
+      #f
+      (let* ((a  (copy (car r) (list n)))
+             (ne (unify (car g) (car a) e)))
+        (if ne
+            (prove3 (append (cdr a) (cdr g)) ne (+ 1 n)))
+        (try g (cdr r) e n))))

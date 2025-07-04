7738d91fcaf5374a122c9b5cdb01bb68dccba27b
Paul Tarvydas <paul.tarvydas@gmail.com>
2025-07-04 15:47:45 -0400

    db

--- prolog-6-modified.scm
+++ prolog-6-modified.scm
@@ -1,18 +1,18 @@
 
-(define (try g r e n)
+(define (try db g r e n)
   (if (null? r)
       #f
       (let* ((a  (copy (car r) (list n)))
              (ne (unify (car g) (car a) e)))
         (if ne
-            (prove3 (append (cdr a) (cdr g)) ne (+ 1 n)))
-        (try g (cdr r) e n))))
+            (prove3 db (append (cdr a) (cdr g)) ne (+ 1 n)))
+        (try db g (cdr r) e n))))
 
-(define (prove3 g e n)
+(define (prove3 db g e n)
   (cond ((null? g)
           (print-frame e))
         (else
-          (try g db e n))))
+          (try db g db e n))))
 
 
 (define link list)
@@ -23,36 +23,38 @@
 (define (L_n x) (car (cddddr x)))
 
 
-(define (back5 l g r e n)
+(define (back5 db l g r e n)
   (if (and (pair? g)
            (pair? r))
-      (prove5 l g (cdr r) e n)
-      (prove5 (L_l l)
+      (prove5 db l g (cdr r) e n)
+      (prove5 db
+	      (L_l l)
               (L_g l)
               (cdr (L_r l))
               (L_e l)
               (L_n l))))
 
 
-(define (prove5 l g r e n)
+(define (prove5 db l g r e n)
   (cond
     ((null? g)
       (print-frame e)
-      (back5 l g r e n))
+      (back5 db l g r e n))
     ((null? r)
       (if (null? l)
           #t
-          (back5 l g r e n)))
+          (back5 db l g r e n)))
     (else
       (let* ((a  (copy (car r) n))
              (e* (unify (car a) (car g) e)))
         (if e*
-            (prove5 (link l g r e n)
+            (prove5 db
+		    (link l g r e n)
                     (append (cdr a) (cdr g))
                     db
                     e*
                     (+ 1 n))
-            (back5 l g r e n))))))
+            (back5 db l g r e n))))))
 
 
 (define (L_c x) (cadr (cddddr x)))
@@ -62,45 +64,47 @@
   (set-car! (cddr x) '(())))
 
 
-(define (back6 l g r e n c)
+(define (back6 db l g r e n c)
   (cond
     ((and (pair? g)
           (pair? r))
-      (prove6 l g (cdr r) e n c))
+      (prove6 db l g (cdr r) e n c))
     ((pair? l)
-      (prove6 (L_l l)
-              (L_g l)
-              (cdr (L_r l))
-              (L_e l)
-              (L_n l)
-              (L_c l)))))
+     (prove6 db
+	     (L_l l)
+             (L_g l)
+             (cdr (L_r l))
+             (L_e l)
+             (L_n l)
+             (L_c l)))))
 
 
-(define (prove6 l g r e n c)
+(define (prove6 db l g r e n c)
   (cond
     ((null? g)
       (print-frame e)
-      (back6 l g r e n c))
+      (back6 db l g r e n c))
     ((eq? '! (car g))
       (clear_r c)
-      (prove6 c (cdr g) r e n c))
+      (prove6 db c (cdr g) r e n c))
     ((eq? 'r! (car g))
-      (prove6 l (cddr g) r e n (cadr g)))
+      (prove6 db l (cddr g) r e n (cadr g)))
     ((null? r)
       (if (null? l)
           #t
-          (back6 l g r e n c)))
+          (back6 db l g r e n c)))
     (else
       (let* ((a  (copy (car r) n))
              (e* (unify (car a) (car g) e)))
         (if e*
-            (prove6 (link l g r e n c)
+            (prove6 db
+		    (link l g r e n c)
                     (append (cdr a) `(r! ,l) (cdr g))
                     db
                     e*
                     (+ 1 n)
                     l)
-            (back6 l g r e n c))))))
+            (back6 db l g r e n c))))))
 
 
 (define empty '((bottom)))
@@ -184,7 +188,7 @@
 
 ;; Graph example from section 1
 
-(define db
+(define db1
   '(((edge a b))
     ((edge a f))
     ((edge a g))
@@ -207,14 +211,14 @@
 (define goals '((path a f (? P))))
 
 ; recursive PROVE
-(prove3 goals empty 1)
+(prove3 db1 goals empty 1)
 
 ; 6-slide PROVE
-(prove5 '() goals db empty 1)
+(prove5 db1 '() goals db1 empty 1)
 
 ;; Negation as failure
 
-(define db
+(define db2
   '(((some foo))
     ((some bar))
     ((some baz))
@@ -231,5 +235,5 @@
                 (neq (? X) (? Y))))
 
 ; 9-slide PROVE
-(prove6 '() goals db empty 1 '())
+(prove6 db2 '() goals db2 empty 1 '())
 
